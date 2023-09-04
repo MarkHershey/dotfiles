e1e31d1f709d87f20389727612bf4f08ef4d0dd4
Mark Huang <hhzeos@gmail.com>
2023-09-04 12:01:50 +0800

    Fix uid bound

--- linux/check-system.sh
+++ linux/check-system.sh
@@ -71,8 +71,8 @@ case $(uname) in
         available_mem=$( free -g | awk '/^Mem:/ {print $7}' )
         total_space=$( df -h --total | grep 'total' | awk '{print $2}' )
         free_space=$( df -h --total | grep 'total' | awk '{print $4}' )
-        num_users=$( awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l )
-        all_users=$( awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | tr '\n' ' ' )
+        num_users=$( awk -F: '$3 >= 1000 && $3 <= 60000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l )
+        all_users=$( awk -F: '$3 >= 1000 && $3 <= 60000 && $1 != "nobody" {print $1}' /etc/passwd | tr '\n' ' ' )
         online_users=$( who | awk '{print $1}' | sort -u | tr '\n' ' ' )
         sudo_users=$( grep -Po '^sudo.+:\K.*$' /etc/group | tr ',' ' ' )
         # check if nvidia-smi is installed
