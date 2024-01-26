fb230651c61c1bbeeaabc2dd99c22774da6c61b0
Mark Huang <hhzeos@gmail.com>
2024-01-26 16:22:23 +0800

    Update

--- nvidia/dl470.sh
+++ nvidia/dl470.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Downloading official installer script for NVIDIA driver 470.223.02 (NVIDIA-Linux-x86_64-470.223.02.run)"
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/470.223.02/NVIDIA-Linux-x86_64-470.223.02.run
---
+++ nvidia/dl535.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Downloading official installer script for NVIDIA driver 535.154.05 (NVIDIA-Linux-x86_64-535.154.05.run)"
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/535.154.05/NVIDIA-Linux-x86_64-535.154.05.run
